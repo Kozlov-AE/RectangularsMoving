516bda7fbf97841a1a875754304e72375e9ea898
Kozlov-AE <Allek-007@list.ru>
2023-02-18 21:27:49 +0300

    Added protos

---
+++ RectangularsMoving.Server/Services/ConfigService.cs
@@ -0,0 +1,16 @@
+using Grpc.Core;
+using RectangularsMoving.Shared.Protos;
+
+namespace RectangularsMoving.Server.Services {
+    public class ConfigService : Config.ConfigBase {
+        // Получаем экземпляр сервиса, который пробегается по хранилищу прямоугольников и меняет их координаты
+        public override Task SetConfig(ConfigRequest request, IServerStreamWriter<Rect> responseStream, ServerCallContext context) {
+            // Иду в фабрику прямоугольников, передаю туда параметры
+            // Подписываюсь на событие создания нового прямоугольника
+            // Как только производится новый прямоугольник, скидываю его в свое хранилище
+            // Отправляю его на клиент (пишу в responseStream)
+            // Если сервис изменения координат не запущен, то запускаю его, передавая ссылку на хранилище и подписываюсь на изменения координат
+            // Получаю новые координаты -> отправляю их клиенту
+        }
+    }
+}
---
+++ RectangularsMoving.Shared/Models/Board.cs
@@ -0,0 +1,7 @@
+namespace RectangularsMoving.Shared.Models {
+    public partial  class Board {
+        int Width {get;set;}
+        int Height {get;set;}
+        int RectsCount {get;set;}
+    }
+}
