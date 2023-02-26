f64345bdf403248b90dcb6ef12eb88796804ec9d
Kozlov-AE <Allek-007@list.ru>
2023-02-26 17:19:58 +0300

    A11 client - fix twise moving

--- RectangularsMoving.AvaloniaClient/AppManager.cs
+++ RectangularsMoving.AvaloniaClient/AppManager.cs
@@ -1,3 +1,4 @@
+using Avalonia.Threading;
 using RectangularsMoving.ClientShared;
 using System;
 using System.Threading.Tasks;
@@ -5,8 +6,13 @@ using System.Threading.Tasks;
 namespace RectangularsMoving.AvaloniaClient {
     public class AppManager :IAppManager {
         public Task RunInUiThreadAsync(Func<Task> action) {
-            action();
-            return Task.CompletedTask;
+            // Avalonia 11 allowed change binded parameters from different threads.
+            // But I've found not expected behavior -
+            // if we change many parameters, it render each of them
+            //
+            //action();
+            //return Task.CompletedTask;
+            return Dispatcher.UIThread.InvokeAsync(action);
         }
     }
 }
